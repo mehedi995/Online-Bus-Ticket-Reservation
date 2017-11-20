16e3c492b511bcd193bf2c16537a53d2f7210ed1
Mehedi Hasan <hmehedi995@gmail.com>
2017-11-20 20:11:15 +0600

    Initial commit

---
+++ Online_Bus_Ticket_Reservation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Online_Bus_Ticket_Reservation.MvcApplication" Language="C#" %>
---
+++ Online_Bus_Ticket_Reservation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_Bus_Ticket_Reservation.Startup))]
+namespace Online_Bus_Ticket_Reservation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
